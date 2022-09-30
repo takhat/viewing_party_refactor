8e4015f99aa460dc09e86f9d0f6f43c285721584
Tapasya Khatri <tapasya@Tapasyas-MacBook-Air.local>
2022-09-30 11:42:29 -0700

    Person class and 1st test

--- tests/test_person.py
+++ tests/test_person.py
@@ -1,9 +1,26 @@
 import pytest
+from viewing_party.person import Person
 
-def test_1():
-    # Arrange
+# def __init__(self, name, watched, friends, subscriptions):
+#         self.name = name
+#         self.watched = watched
+#         self.friends = friends
+#         self.subscriptions = subscriptions
 
-    # Act
+#     def add_watched(self, movie):
+#         self.watched.append(movie)
+#         return self.watched
+    
+#     def get_num_watched(self):
+#         return len(self.watched)
+def test_1_create_person_valid_inputs():
+    # Arrange and Act
+    person1 = Person("Cheetah",["Titanic", "Top Gun"], ["Lion", "Tiger"], ["Hulu", "HBX", "Netflix"])
 
     # Assert
-    pass
+    assert person1.name == "Cheetah"
+    assert person1.watched == ["Titanic", "Top Gun"]
+    assert person1.friends == ["Lion", "Tiger"]
+    assert person1.subscriptions == ["Hulu", "HBX", "Netflix"]
+    
+
